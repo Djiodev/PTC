87c11329d80b61d2b9efc0fd89c96a089e96d00c
Djio <g.zarrella@hotmail.it>
2016-12-28 04:26:01 +0000

    api working

--- PTC/Scripts/Controllers/cyclingController.js
+++ PTC/Scripts/Controllers/cyclingController.js
@@ -13,6 +13,7 @@
 
         vm.trips = [];
         vm.stops = [];
+        vm.stopStats = [];
 
         vm.tripName = "";
 
@@ -24,7 +25,7 @@
             });
 
         vm.showStops = function (trip) {
-            alert(trip.name + ", " + trip.username);
+            console.log(trip.name + ", " + trip.username);
             $http.get("http://cyclingtrips.net/api/allTrips/" + trip.name +
                 "/" + trip.username + "/stops")
                 .then(function (response) {
@@ -34,7 +35,40 @@
                 });
         };
 
+        vm.stats = function () {
+            getStats(vm.stops);
+            setTimeout(function () {
+                for (var i = 0; i < vm.stopStats.origin_addresses.length; i++) {
+                    console.log(vm.stopStats.origin_addresses[i] + " to " + vm.stopStats.destination_addresses[i] +
+                    ": " + vm.stopStats.rows[i].elements[i].distance.text + "\r\n");
+                    };
+                //console.log(vm.stopStats.origin_addresses[0] + " to " + vm.stopStats.destination_addresses[0] +
+                //    ": " + vm.stopStats.rows[0].elements[0].distance.text + "\r\n" +
+                //    vm.stopStats.origin_addresses[1] + " to " + vm.stopStats.destination_addresses[1] +
+                //    ": " + vm.stopStats.rows[1].elements[1].distance.text);
+            }, 1000);
+        };
 
+        var getStats = function (stops) {
+            var origins = stops[0].location;
+            var destinations = stops[1].location;
+            for (var i = 1; i < stops.length - 1; i++) {
+                origins += "|" + stops[i].location;
+                destinations += "|" + stops[i + 1].location;
+            };
+
+            console.log("origins: " + origins + "\r\ndestinations: " + destinations);
+
+            var tripStats = [];
+            $http.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=" + origins +
+            "&destinations=" + destinations + "&mode=driving&key=AIzaSyDLEU3W2SbHKPHzQrGvzsfwRz5SUm8IIHc")
+            .then(function (response) {
+                angular.copy(response.data, vm.stopStats);
+            }, function (err) {
+                alert("fail google maps");
+            });
+
+        };
 
     }
 
